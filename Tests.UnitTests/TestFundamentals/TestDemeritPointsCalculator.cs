f9b9dc96ea3daff29c9dfa3b887192f4e3e6bb01
zeynab <z.rostami76@gmail.com>
2025-09-11 06:38:10 -0700

    twe examples

---
+++ Tests.UnitTests/TestFundamentals/TestDemeritPointsCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Src.UnitTests.Fundamentals;
+using Xunit;
+
+namespace Tests.UnitTests.TestFundamentals
+{
+    public class TestDemeritPointsCalculator
+    {
+        public void CalculateDemeritPoints_SpeedBelowOrEqualToLimit_ReturnsZero() 
+        {
+            //Arrange
+
+            var demeritpoints=new DemeritPointsCalculator();
+
+            //Act
+            var result = demeritpoints.CalculateDemeritPoints(65);
+
+            //Assert
+            Assert.Equal(0,result);
+
+        }
+    }
+}
