c7333a3c71c3decc8c36bf706decd70d6d8bdf30
z.rostami <z.rostami@pardis.ir>
2025-10-04 16:33:17 +0330

    add payment

---
+++ Tests.Src.UnitTests/Dependencies/PaymentMethod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Src.UnitTests.Dependencies
+{
+    public  abstract class PaymentMethod
+    {
+        public abstract bool Process(decimal amount);
+    }
+    public class CarditCardPayment : PaymentMethod
+    {
+        public override bool Process(decimal amount)
+        {
+            return true; 
+        }
+    }
+    public class PayPalPaymentMethod : PaymentMethod
+    {
+        public override bool Process(decimal amount)
+        {
+            return true; 
+        }
+    }
+}
